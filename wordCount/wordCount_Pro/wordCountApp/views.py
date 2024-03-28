c693c0945d15459930f0414a0761cd47294f4492
hyunnjjung <hyunnjjung@gmail.com>
2024-03-28 15:10:21 +0900

    1차 스터디

--- wordCount/wordCount_Pro/wordCountApp/views.py
+++ wordCount/wordCount_Pro/wordCountApp/views.py
@@ -13,6 +13,17 @@ def result(request):
     entered_text = request.GET['fulltext']
     word_list = entered_text.split()
 
+    #전체 글자 수
+    characters = len(entered_text)
+
+    #띄어쓰기 제외 글자수
+    characters_nospace = characters
+
+    for i in range(characters_nospace):
+        if ' ' in entered_text[i]:
+            characters_nospace -= 1
+
+
     word_dictionary = {}
 
     for word in word_list:
@@ -21,7 +32,7 @@ def result(request):
         else:
             word_dictionary[word] = 1
 
-    return render(request, "result.html", {'word_list': word_list, 'alltext': entered_text, 'dictionary': word_dictionary.items()}, )
+    return render(request, "result.html", {'characters' : characters, 'characters_nospace' : characters_nospace, 'word_list': word_list, 'alltext': entered_text, 'dictionary': word_dictionary.items()}, )
 
 def hello(request):
     entered_name = request.GET['myname']
