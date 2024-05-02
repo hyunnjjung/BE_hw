94ade0bb550bb6898092da256fd0d6043e62b1db
hyunnjjung <hyunnjjung@gmail.com>
2024-05-02 15:00:42 +0900

    [4차 스터디] 연락처 CRUD기능 추가

--- Phone/Phone_Prj/contacts/urls.py
+++ Phone/Phone_Prj/contacts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import *
+from .views import create, detail, update, delete, List, Result
 
 urlpatterns = [
     path('', List.as_view(), name = "list"),
     path('result', Result.as_view(),  name = "result"),
+    path('create/', create, name = "create"),
+    path('detail/<int:id>/', detail, name = "detail" ),
+    path('update/<int:id>/', update, name = "update"),
+    path('delete/<int:id>', delete, name = "delete"),
 ]
 
 
--- Phone/Phone_Prj/contacts/views.py
+++ Phone/Phone_Prj/contacts/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic import ListView
 from .models import Phone
 
@@ -19,6 +20,44 @@ class Result(ListView):
         context = super().get_context_data(**kwargs)
         context['data'] = self.data
         return context
+    
+def create(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        phone_num = request.POST.get('phone_num')
+        email = request.POST.get('email')
+
+        contact = Phone.objects.create(
+            name = name,
+            phone_num = phone_num,
+            email = email
+        )
+
+        return redirect('list')
+    return render(request, 'contacts/create.html')
+
+def detail(request, id):
+    contact = get_object_or_404(Phone, id = id)
+    return render(request, 'contacts/detail.html', {'contact' :contact})
+
+def update(request, id):
+    contact = get_object_or_404(Phone, id = id)
+    if request.method == "POST":
+        contact.name = request.POST.get('name')
+        contact.phone_num = request.POST.get('phone_num')
+        contact.email = request.POST.get('email')
+        contact.save()
+        return redirect('list')
+    return render(request, 'contacts/update.html', {'contact' :contact})
+    
+
+def delete(request, id):
+    contact = get_object_or_404(Phone, id = id)
+    if request.method == "POST":
+        contact.delete()
+        return redirect('list')
+    return render(request, 'contacts/delete.html', {'contact' :contact})
+    
 
 
 
